587d6444d54c5003bd9e9f369064603fea34b621
Jeferson <jefer.ls@hotmail.com>
2019-06-24 19:39:04 -0300

    testando

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,23 +2,23 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        //$this->call(UsuariosSeeder::class);
-        //$this->call(EmailsSeeder::class);
-        //$this->call(VotosSeeder::class);
-        //$this->call(BooksSeeder::class);
-        //$this->call(LocalSeeder::class);
-        //$this->call(EstadosSeeder::class);
-        //$this->call(CidadesSeeder::class);
-        //$this->call(BeneficiariosSeeder::class);
+    public function run() {
+        $this->call(UsuariosSeeder::class);
+        $this->call(EmailsSeeder::class);
+        $this->call(VotosSeeder::class);
+        $this->call(BooksSeeder::class);
+        $this->call(LocalSeeder::class);
+        $this->call(EstadosSeeder::class);
+        $this->call(CidadesSeeder::class);
+        $this->call(BeneficiariosSeeder::class);
         $this->call(PostsSeeder::class);
     }
+
 }
